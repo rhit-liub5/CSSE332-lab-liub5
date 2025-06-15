2cd409c3f975d669ab048ba6e4d8094f0069efd2
Your Name <you@example.com>
2025-06-15 19:29:30 -0400

    测试

--- simpleshell/donothing.c
+++ simpleshell/donothing.c
@@ -10,6 +10,6 @@ int main(int argc, char** argv) {
     }
     printf("Pausing for 5 seconds...\n");
     sleep(5);
-    printf("Do nothing program finished!\n");
+    printf("Do nothing program %d finished!\n", getpid());
     return 0;
 }
